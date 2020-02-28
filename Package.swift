80bd7d693581a1ec688f79ddcbf84f165f5a1040
araki <araki@qoncept.co.jp>
2020-02-28 10:43:37 +0900

    Update deps

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "VGAN",
     dependencies: [
-        .package(url: "https://github.com/t-ae/gan-utils-s4tf.git", from: "0.2.4"),
+        .package(url: "https://github.com/t-ae/gan-utils-s4tf.git", from: "0.2.5"),
         .package(url: "https://github.com/t-ae/image-loader.git", from: "0.1.8"),
         .package(url: "https://github.com/t-ae/tensorboardx-s4tf.git", from: "0.1.0"),
     ],
