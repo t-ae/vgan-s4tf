d2e183f3b0e2d83a397dacc3b4301d70c455f475
araki <araki@qoncept.co.jp>
2020-03-06 10:20:06 +0900

    Update Package.swfit

--- Package.swift
+++ Package.swift
@@ -1,14 +1,17 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "VGAN",
+    platforms: [
+        .macOS(.v10_13),
+    ],
     dependencies: [
-        .package(url: "https://github.com/t-ae/gan-utils-s4tf.git", from: "0.3.1"),
-        .package(url: "https://github.com/t-ae/image-loader.git", from: "0.1.8"),
-        .package(url: "https://github.com/t-ae/tensorboardx-s4tf.git", from: "0.1.1"),
+        .package(name: "GANUtils", url: "https://github.com/t-ae/gan-utils-s4tf.git", from: "0.3.6"),
+        .package(name: "ImageLoader", url: "https://github.com/t-ae/image-loader.git", from: "0.1.9"),
+        .package(name: "TensorBoardX", url: "https://github.com/t-ae/tensorboardx-s4tf.git", from: "0.1.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
